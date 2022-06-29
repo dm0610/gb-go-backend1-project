dd70a3875d0678a8a89b0708ae85804b031e9678
Стрельников Дмитрий Валерьевич <dmvstrelnikov@beeline.ru>
2022-06-29 22:16:45 +0300

    upd

--- product_builder/main_test.go
+++ product_builder/main_test.go
@@ -18,6 +18,8 @@ var a App
 
 func TestMain(m *testing.M) {
 	a.Initialize(
+		os.Getenv("APP_DB_PORT"),
+		os.Getenv("APP_DB_HOST"),
 		os.Getenv("APP_DB_USERNAME"),
 		os.Getenv("APP_DB_PASSWORD"),
 		os.Getenv("APP_DB_NAME"))
